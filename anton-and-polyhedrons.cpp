b4f162424d95a8a2a12b7f78881998f23cde5186
Mohamed Yamani <elamine.yamani@gmail.com>
2020-11-18 11:59:03 +0100

    Learned about switch statements in C++. A bit complicated to use on strings

---
+++ anton-and-polyhedrons.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	int n, faces = 0;
+	string s;
+	cin >> n;
+	for (int i = 0; i < n; ++i){
+		cin >> s;
+		if (s == "Tetrahedron"){
+			faces += 4;
+		}
+		else if (s == "Cube"){
+			faces += 6;
+		}
+		else if (s == "Octahedron"){
+			faces += 8;
+		}
+		else if (s == "Dodecahedron"){
+			faces += 12;
+		}
+		else {
+			faces += 20;
+		}
+	}
+	cout << faces;
+	return 0;
+}
