0b6206a81862a54a18163d69c67b05702cb4f1df
Mohamed Yamani <elamine.yamani@gmail.com>
2020-11-25 10:57:27 +0100

    Solved it

--- new-year-and-hurry.cpp
+++ new-year-and-hurry.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 int main(){
-	int n, k, x = 0;
-	cin >> n >> k;
-	k = 240 - k; //available time to solve
-	for (int i = 1; i <= n; ++i){
-		x += 5*i;
-		if (x >= k){
-			if (x == k){
-				cout << i;
-			}
-			else{
+	int probs, time, x = 0;
+	cin >> probs >> time;
+	time = 240 - time; //available time to solve
+	if (((5*probs)*(1+probs))/2 <= time){
+		cout << probs;
+	}
+	else{
+		for (int i = 1; i <= probs; ++i){
+			x += 5*i;
+			if (x > time){
 				cout << i-1;
 				break;
 			}
